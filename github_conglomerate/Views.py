5042d20cba643faf31af897634e024ba9a328325
Matthew Astley <mca@sanger.ac.uk>
2015-04-02 14:33:54 +0100

    better get_count_points

--- github_conglomerate/Views.py
+++ github_conglomerate/Views.py
@@ -66,26 +66,10 @@ class Repos(object):
       return 0
 
   def get_count_points(self, count):
-    if count > 100:
-      # No more than 10 points
-      return 10
-    elif count > 50:
-      # 7 to 10 points
-      return 7 + 3 * (float(count) - 50) / 50
-    elif count > 10:
-      # 5 to 10 points
-      return 5 + 2 * (float(count) - 10) / 40
-    elif count > 5:
-      # 3 to 5 points
-      return 3 + 2 * (float(count) - 5) / 2
-    elif count > 4:
-      return 2.5
-    elif count > 3:
-      return 2
-    elif count > 0:
-      return 1
-    else:
-      return 0
+    return int(log(abs(i)+1)*2.1668*100 + 0.5)/100.0
+    # abs(i) + 1 : now we can work on any int
+    # 2.1668     : factor to match previous step algorithm at 100 => 10.0
+    # then round to 2dp
 
   def sorted_by(self, repos, *args):
     repos = repos[:]
